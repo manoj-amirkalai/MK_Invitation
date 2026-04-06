a1bcfbe2c8ec87442c907615a7958aaac3fcd0ab
Manoj.A <manojamir2911@gmail.com>
2026-04-06 20:50:08 +0530

    images upload

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import Countdown from "./components/Countdown/Countdown";
 import Header from "./components/Header/Header";
 import "./main.css";
 import DomeGallery from "./components/css/DomeGallery/DomeGallery";
+import LocationDetails from "./components/LocationDetails/LocationDetails";
+import WeddingFooter from "./components/WeddingFooter/WeddingFooter";
 
 const style = {
   width: 480,
@@ -24,15 +26,17 @@ createRoot(document.getElementById("root")).render(
       <Temple />
       <Countdown />
       <div style={{ width: 480, height: "50vh" }}>
-      <DomeGallery
-  fit={0.8}
-  minRadius={600}
-  maxVerticalRotationDeg={0}
-  segments={34}
-  dragDampening={2}
-  grayscale={false}
-/>
+        <DomeGallery
+          fit={0.8}
+          minRadius={600}
+          maxVerticalRotationDeg={0}
+          segments={34}
+          dragDampening={2}
+          grayscale={false}
+        />
       </div>
+      <LocationDetails/>
+      <WeddingFooter/>
     </div>
   </StrictMode>,
 );
